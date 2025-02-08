c2e57865a27fcf0831e88472732e44476698b9de
kirsan <k.shalhakov@yandex.ru>
2025-02-08 09:08:23 +0200

    modified by linters

--- src/widget.py
+++ src/widget.py
@@ -1,20 +1,24 @@
-def mask_account_card(info):
+from datetime import datetime
+
+
+def mask_account_card(info: str) -> str:
     if info.startswith("Счет"):
-        # Маскировка для счета
-        return 'Cчет **' + info[-4:]
+        return "Cчет **" + info[-4:]
     else:
-        # Маскировка для карт
         parts = info.split()
-        card_type = ' '.join(parts[:-1])
+        card_type = " ".join(parts[:-1])
         card_number = parts[-1]
         return f"{card_type} {card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
 
+    """ Функция для маскирования номера счета и карты"""
 
 
-def get_date(date_string):
-    from datetime import datetime
+def get_date(date_string: str) -> str:
     date_time_obj = datetime.fromisoformat(date_string)
     return date_time_obj.strftime("%d.%m.%Y")
+
+    """Функция для реобразования даты в формат 'ДД.ММ.ГГГГ'"""
+
 
 print(mask_account_card("Maestro 1596837868705199"))
 print(mask_account_card("Счет 35383033474447895560"))
