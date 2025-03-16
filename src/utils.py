3a85da3a6f5d5e2b88248a520e5cb350ed611fdf
kirsan <k.shalhakov@yandex.ru>
2025-03-16 20:05:50 +0200

    homework12.1

---
+++ src/utils.py
@@ -0,0 +1,35 @@
+import json
+from typing import Any, Dict, List
+
+from .external_api import convert_to_rub
+
+
+def load_transactions(filename: str) -> List[Dict[str, Any]]:
+    """
+    Загрущк транзакции из указанного файла в формате JSON.
+    """
+    with open(filename, 'r', encoding='utf-8') as file:
+        return json.load(file)
+
+
+def main() -> None:
+    """
+    Основная функция, загружающая транзакции и конвертирующая суммы в рубли.
+    """
+    transactions: List[Dict[str, Any]] = load_transactions('operations.json')
+    for transaction in transactions:
+        try:
+            print("Available keys in transaction:", transaction.keys())
+            if 'operationAmount' in transaction and 'amount' in transaction['operationAmount']:
+                amount = transaction['operationAmount']['amount']
+                currency = transaction['operationAmount']['currency']['code']
+                amount = float(amount)
+                amount_in_rub = convert_to_rub(amount, currency)
+                print(f"Transaction ID: {transaction['id']}, Amount in RUB: {amount_in_rub:.2f}")
+            else:
+                print(
+                    f"Transaction ID: {transaction.get('id', 'unknown')} does not contain"
+                    f" 'operationAmount' or 'amount' information.")
+
+        except Exception as e:
+            print(f"Failed to process transaction ID: {transaction.get('id', 'unknown')}. Error: {e}")
